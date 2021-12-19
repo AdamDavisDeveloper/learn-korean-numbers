6fb937bcd95913ab4dee326c52d9af6aec7e286f
Adam Davis <adavisdeveloper@gmail.com>
2021-12-19 17:36:34 -0500

    it works! just needs clean up :)

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-/* 
-~~ plan ~~
+const koreanSet = ["", "일", "이", "삼", "사", "오", "룍", "칠", "팔", "구"];
 
-1. Generate a random number between 1-9999
-2. Convert that number to sino-korean by:
-    -Finding how many times 1000 can go into that number (if 0, move on)
-    -Finding how many times 100 can go into that number (if 0, move on)
-    -Finding how many times 10 can go into that number (if 0, move on)
-    -Finding the remainder after the number is divided by 10
+const koreanTen = "십";
+const koreanOneHundred = "백";
+const koreanOneThousand = "천";
 
-3. Take the number of 1000s, 100s, 10s (if any) and convert that to Korean followed
-   by the 1000/100/10 korean word
-4. Add the remainder korean number to the end 
-*/
-
-const testNum = 112;
-
-const englishKoreanSet = [
-  "",
-  "일",
-  "이",
-  "삼",
-  "사",
-  "오",
-  "룍",
-  "칠",
-  "팔",
-  "십",
-];
-
-const oneHundred = "백";
-const oneHundred = "천";
+let remainingNumber;
 
 function foldWay(num, fold) {
-  //returns number of times "num" goes into "fold"
-  return Math.floor(num / fold);
+	//returns number of times "num" goes into "fold"
+	return Math.floor(num / fold);
 }
 
 function getSinoKorean() {
-  const hundreds = foldWay(randInt, 100);
-  const tens = foldWay(randInt, 10);
-
-  return;
+	remainingNumber = Math.floor(Math.random() * (9999 - 1 + 1) + 1);
+	console.log(remainingNumber);
+	return `${getThousands(remainingNumber)}${getHundreds(
+		remainingNumber
+	)}${getTens(remainingNumber)}${koreanSet[remainingNumber]}`;
 }
 
-console.log(foldWay(testNum, 10));
+function getThousands(int) {
+	const thousands = foldWay(int, 1000);
+	if (thousands) {
+		remainingNumber = remainingNumber % 1000;
+		if (thousands == 1) return koreanOneThousand;
+		else return `${koreanSet[thousands]}${koreanOneThousand}`;
+	} else return "";
+}
+
+function getHundreds(int) {
+	const hundreds = foldWay(int, 100);
+	if (hundreds) {
+		remainingNumber = remainingNumber % 100;
+		if (hundreds == 1) return koreanOneHundred;
+		else return `${koreanSet[hundreds]}${koreanOneHundred}`;
+	} else return "";
+}
+
+function getTens(int) {
+	const tens = foldWay(int, 10);
+	if (tens) {
+		remainingNumber = remainingNumber % 10;
+		if (tens == 1) return koreanTen;
+		else return `${koreanSet[tens]}${koreanTen}`;
+	} else return "";
+}
