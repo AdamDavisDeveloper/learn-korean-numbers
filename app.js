406eb5dfabb7cd16178908dea6dbf71051c20f30
Adam Davis <adavisdeveloper@gmail.com>
2021-12-20 12:25:52 -0500

    start up the UI for the card

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const koreanSet = ["", "일", "이", "삼", "사", "오", "룍", "칠", "팔", "구"];
-
 const koreanTen = "십";
 const koreanOneHundred = "백";
 const koreanOneThousand = "천";
@@ -7,20 +6,19 @@ const koreanOneThousand = "천";
 let remainingNumber;
 
 function foldWay(num, fold) {
-	//returns number of times "num" goes into "fold"
 	return Math.floor(num / fold);
 }
 
 function getSinoKorean() {
 	remainingNumber = Math.floor(Math.random() * (9999 - 1 + 1) + 1);
 	console.log(remainingNumber);
-	return `${getThousands(remainingNumber)}${getHundreds(
-		remainingNumber
-	)}${getTens(remainingNumber)}${koreanSet[remainingNumber]}`;
+	return `${getThousands()}${getHundreds()}${getTens()}${
+		koreanSet[remainingNumber]
+	}`;
 }
 
-function getThousands(int) {
-	const thousands = foldWay(int, 1000);
+function getThousands() {
+	const thousands = foldWay(remainingNumber, 1000);
 	if (thousands) {
 		remainingNumber = remainingNumber % 1000;
 		if (thousands == 1) return koreanOneThousand;
@@ -28,8 +26,8 @@ function getThousands(int) {
 	} else return "";
 }
 
-function getHundreds(int) {
-	const hundreds = foldWay(int, 100);
+function getHundreds() {
+	const hundreds = foldWay(remainingNumber, 100);
 	if (hundreds) {
 		remainingNumber = remainingNumber % 100;
 		if (hundreds == 1) return koreanOneHundred;
@@ -37,11 +35,15 @@ function getHundreds(int) {
 	} else return "";
 }
 
-function getTens(int) {
-	const tens = foldWay(int, 10);
+function getTens() {
+	const tens = foldWay(remainingNumber, 10);
 	if (tens) {
 		remainingNumber = remainingNumber % 10;
 		if (tens == 1) return koreanTen;
 		else return `${koreanSet[tens]}${koreanTen}`;
 	} else return "";
 }
+
+document.getElementById("GenerateBtn").addEventListener("click", () => {
+	document.getElementById("CardTextFront").innerText = getSinoKorean();
+});
