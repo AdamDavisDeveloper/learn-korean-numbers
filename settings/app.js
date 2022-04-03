663da25441de829d53b3d0488fc6abb47d26d837
Adam Davis <adavisdeveloper@gmail.com>
2022-04-03 16:06:31 -0400

    toggles now work with persistent localStorage state

--- settings/app.js
+++ settings/app.js
@@ -3,5 +3,17 @@ const NumberSystemSwitch = document.getElementById("NumberSystemSwitch");
 const ColorSwitch = document.getElementById("ColorSwitch");
 
 NumberSystemSwitch.addEventListener("click", (e) => {
-	console.log(e.target.checked);
+	localStorage.setItem("number_system", e.target.checked);
 });
+
+ColorSwitch.addEventListener("click", (e) => {
+	localStorage.setItem("colorize", e.target.checked);
+});
+
+function checkedLocal_System() {
+	return localStorage.getItem("number_system") || false;
+}
+
+function checkedLocal_Color() {
+	return localStorage.getItem("colorize") || false;
+}
